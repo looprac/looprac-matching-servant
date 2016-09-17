e3e91e23789e0d50b38517bd93df989e914a1dc5
Hongbo Zhang <hongbo.zhang@uwaterloo.ca>
2016-09-17 16:31:08 -0400

    create google map service; update vendor

---
+++ direction.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"golang.org/x/net/context"
+	"googlemaps.github.io/maps"
+)
+
+var mapclient *(maps.Client)
+
+func initClient() *(maps.Client) {
+	client, err := maps.NewClient(maps.WithAPIKey(os.Getenv("GOOGLE_API_KEY")))
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
+	mapclient = client
+	return client
+}
+
+func getTravelTime(origin_alt, origin_lng, destin_alt, destin_lng float64) float64 {
+	request := &maps.DirectionsRequest{
+		Origin:      fmt.Sprintf("%f,%f", origin_alt, origin_lng),
+		Destination: fmt.Sprintf("%f,%f", destin_alt, destin_lng),
+	}
+	responses, _, err := mapclient.Directions(context.Background(), request)
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
+	return responses[0].Legs[0].Duration.Minutes()
+}
